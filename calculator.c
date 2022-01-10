4fbe01a4f50316ca4b34be52cf588d562c85139e
rejlive <72590228+rejlive@users.noreply.github.com>
2022-01-10 00:35:06 -0500

    Add files via upload

--- calculator.c
+++ calculator.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 int main() {
+	// Character assigned to an operator 
   char op;
   double first, second;
   printf("Enter an operator (+, -, *, /): ");
   scanf("%c", &op);
+  // This function wants two inputs for the operands 
   printf("Enter two operands: ");
   scanf("%lf %lf", &first, &second);
-
+	/* The output will depend on the operator chosen along with the operands.
+	This section contains functions that will ask for the operator that will
+	be input. It also compares the operands and the operator against each other */
   switch (op) {
     case '+':
       printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
@@ -20,7 +24,7 @@ int main() {
     case '/':
       printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
       break;
-    // operator doesn't match any case constant
+    // operator does not match any case constant 
     default:
       printf("Error! operator is not correct");
   }
